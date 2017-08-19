71eff073e3ce2e968eba33fa1094397b4bac631d
rmit-s3579161-loso-lai <s3579161@student.rmit.edu.au>
2017-08-19 15:22:08 +1000

    sample code_ArrayAdapter

--- app/src/main/java/com/example/loso/myapplication/MainActivity.java
+++ app/src/main/java/com/example/loso/myapplication/MainActivity.java
@@ -1,13 +1,19 @@
 package com.example.loso.myapplication;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.app.Activity;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        String[] test = {"AAA", "BBB", "CCC", "DDD", "EEE", "FFF"};
+        ListView lvContact = (ListView)findViewById(R.id.contact);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(MainActivity.this, android.R.layout.simple_list_item_1, test);
+        lvContact.setAdapter(adapter);
     }
 }
